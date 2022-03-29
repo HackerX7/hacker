cd8c1c26b705e9f73cc3541993ef4dee67aacc54
Hacker Jr <70173542+HackerX7@users.noreply.github.com>
2022-03-29 23:21:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sreejithmadmax/deleterobot.git /deleterobot
+  git clone https://github.com/HackerX7/hacker.git /hacker
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /deleterobot
+  git clone $UPSTREAM_REPO /hacker
 fi
-cd /deleterobot
+cd /hacker
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
